1ee40035ce33c6a5a0335c1da1c7b48b0c29554e
rodrigojat <rodrigojat@hotmail.com>
2020-11-09 11:57:23 -0300

    Create main.sh

---
+++ trabalhoEDL/03/main.sh
@@ -0,0 +1,3 @@
+cat resultado.txt | grep "Rajad Adu" | tee file2.txt | wc -l
+
+# Esse comando procura a string passada e armazena no arquivo file2.txt. Além disso ele printa na tela o número de linhas que possuem a string.
